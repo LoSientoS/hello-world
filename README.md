3f2aaac1e675a560a55682c28bc7f23c21dca587
ytee <yxtee0824@gmail.com>
2025-02-11 20:02:00 -0500

    Cleansed everything

--- README.md
+++
@@ -1,2 +0,0 @@
-This is just me noodling around.
-Merging branches for the first time.
--- helloworld.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-	std::cout << "Hello world!\n";
-	std::cout << "I'm feeling a little poopy right now!\n";
-	return 0;
-}
